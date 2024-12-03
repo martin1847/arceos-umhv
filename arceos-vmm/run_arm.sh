2d281ac758cf8d9c51d73ce493b3938ef9d8f08e
martin.cong <martin.cong@zhulinkeji.com>
2024-12-03 23:15:55 +0800

    arm

--- arceos-vmm/run_arm.sh
+++ arceos-vmm/run_arm.sh
@@ -1,2 +1,5 @@
 # make ACCEL=n ARCH=aarch64 LOG=debug VM_CONFIGS=configs/nimbos-aarch64.toml run
-make ACCEL=n ARCH=riscv64 LOG=info VM_CONFIGS=configs/arceos-riscv64.toml run
+# make ACCEL=n ARCH=riscv64 LOG=info VM_CONFIGS=configs/arceos-riscv64.toml run
+
+# ACCEL=y ARCH=x86_64
+make ACCEL=n ARCH=aarch64 LOG=info VM_CONFIGS=configs/arceos-aarch64.toml run
